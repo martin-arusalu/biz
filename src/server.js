1470ed871849c6db0d492456d45bb239373cabd1
Martin Arusalu <martin.arusalu@gmail.com>
2019-08-12 21:01:55 +0300

    Reset script for testing

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ import usersRoutes from './routes/users';
 import companiesRoutes from './routes/companies';
 import productsRoutes from './routes/products';
 import productEntitiesRoutes from './routes/product-entities';
+import ProductEntity from './models/ProductEntity';
+import Product from './models/Product';
+import Company from './models/Company';
+import User from './models/User';
 
 mongoose.connect(env.dbUrl)
 const db = mongoose.connection;
@@ -22,6 +26,25 @@ db.once('open', function () {
   app.use('/companies', companiesRoutes);
   app.use('/products', productsRoutes);
   app.use('/product-entities', productEntitiesRoutes);
+
+
+  app.get("/reset-with-test", async (req, res) => {
+    // clear
+    ProductEntity.deleteMany();
+    Product.deleteMany();
+    Company.deleteMany();
+    User.deleteMany();
+
+    // create
+    const user = await User.create({ name: 'kalle', password: 'password'});
+    const company = await Company.create({ owner: user._id, name: 'Test company' });
+    const product = await Product.create({ name: 'Lemon water' });
+    const companyProduct = await ProductEntity.create({ company: company._id, product: product._id, quality: 50 });
+
+    console.log(companyProduct);
+
+    res.send('ok');
+  });
 
   app.listen(env.port, () => console.log(`Visit ${env.url}:${env.port}`));
 });
